a041ee7fd7dad3b4f7b842857a62ef67cca47a33
caillol <florian-caillol@hotmail.Fr>
2017-12-19 11:51:37 +0100

    Ajout de la gestion de l'assechement en plus dans l'ingénieur

--- src/Aventurier/Ingenieur.java
+++ src/Aventurier/Ingenieur.java
@@ -6,10 +6,18 @@ import main.main.Tuile;
 
 import main.main.Utils.Pion;
 public class Ingenieur extends Aventurier {
-
+    private boolean assechementBonus=false;
     public Ingenieur(Tuile tuile, String nom) {
         super(tuile, nom);
         setNomRole(NOM_AVENTURIER.INGENIEUR);
         setPion(Pion.ROUGE);
+    }
+    @Override
+    public void setAssechementBonus(boolean b){
+        assechementBonus=b;
+    }
+    @Override
+    public boolean getAssechementBonus(){
+        return assechementBonus;
     }
 }
